58383143c902c0361ee87e9592148b14b78a5e25
Mae Duphorne <maelduphorne@gmail.com>
2021-08-25 15:33:37 -0600

    Revise data set

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import movieData from '../../movieData';
+import MoviesArea from '../MoviesArea/MoviesArea'
 // import Ideas from './Components/Ideas';
 // import Form from './Components/Form';
 import './App.css';
@@ -8,7 +9,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [movieData.movies]
+      movies: movieData.movies
     }
   }
 
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <main className='App'>
         <h1>Rancid Tomatillos</h1>
+        <MoviesArea movies={this.state.movies}/>
       </main>
     )
   }
