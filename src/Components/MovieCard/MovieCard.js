efa3935c5ad97f5683b4c837f9f9a3a15473aea7
Mae Duphorne <maelduphorne@gmail.com>
2021-08-25 19:00:09 -0600

    Create grid display

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -5,11 +5,15 @@ const MovieCard = ({ title, poster, rating, id }) => {
   // console.log(MoviesArea)
   return (
     <div className='card'>
-      <h2>{title}</h2>
-      <p>{rating}</p>
-      <img src={poster}/>
+      <img class='poster' src={poster}/>
     </div>
   )
 }
 
 export default MovieCard
+
+
+
+//IN MOVIE CARD:
+// <h2>{title}</h2>
+// <p>{rating}</p>
