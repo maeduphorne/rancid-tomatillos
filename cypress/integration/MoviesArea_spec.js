da8f14d0758d831617d8d82a6585ab39e8da4f57
darlaevans2000 <darlaevans2000@gmail.com>
2021-08-30 16:00:55 -0600

    Add base working tests

---
+++ cypress/integration/MoviesArea_spec.js
@@ -0,0 +1,9 @@
+describe('Movie area flows', () => {
+    beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should confirm that true is equal to true', () => {
+    expect(true).to.equal(true)
+  });
+});
