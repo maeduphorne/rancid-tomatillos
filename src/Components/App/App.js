76853cf6b545de5b04ede28cc10de46453811e6a
darlaevans2000 <darlaevans2000@gmail.com>
2021-08-30 15:10:04 -0600

    Change error key to error

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,14 +12,14 @@ class App extends Component {
       movies: [],
       selectedMovie: '',
       movieTrailer: '',
-      errorKey: ''
+      error: ''
     }
   }
 
   componentDidMount = () => {
     APICalls.fetchMoviesData()
     .then(data => this.setState({movies:[ ...this.state.movies,...data.movies]}))
-    .catch(error => this.setState({errorKey: 'Oops! Looks like something went wrong'}))
+    .catch(error => this.setState({error: 'Oops! Looks like something went wrong'}))
   }
 
 // Fetch single movie data
@@ -30,11 +30,11 @@ class App extends Component {
     // this.state.movies.find(movie => movie.id === id);
     APICalls.fetchSingleMovieData(id)
     .then(data => this.state.selectedMovie ? this.setState({selectedMovie: ''}) : this.setState({selectedMovie: data}))
-    .catch(error => this.setState({errorKey: 'Oops! We are unable to display this movie'}))
+    .catch(error => this.setState({error: 'Oops! We are unable to display this movie'}))
 
     APICalls.fetchMovieVideoData(id)
     .then(data => this.setState({movieTrailer: data.videos}))
-    .catch(error => this.setState({errorKey: 'Oops! We are unable to dsiplay this trailer'}))
+    .catch(error => this.setState({error: 'Oops! We are unable to dsiplay this trailer'}))
     // this.setState({selectedMovie: data})
     //if the length is 0 then keep empty, if not, set state to the clicked movie
   }
@@ -49,7 +49,7 @@ class App extends Component {
     return (
       <main className='App'>
         <h1>Rancid Tomatillos</h1>
-        {this.state.errorKey && <h2>{this.state.errorKey}</h2>}
+        {this.state.error && <h2>{this.state.error}</h2>}
         {!this.state.selectedMovie && <MoviesArea movies={this.state.movies} displayMovie={this.displayMovie}/>}
         {this.state.selectedMovie && <MovieInfo selectedMovie={this.state.selectedMovie.movie} movieTrailer={this.state.movieTrailer} displayHomePage={this.displayHomePage}/>}
       </main>
