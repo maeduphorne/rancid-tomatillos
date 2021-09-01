cb7f0f9a352bc76ebba2d53b1af3edb7975f2dd8
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-01 11:21:07 -0600

    Change jsx files back to js

--- src/Components/MovieInfo/MovieInfo.js
+++ src/Components/MovieInfo/MovieInfo.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import APICalls from '../API/APICalls';
 
 class MovieInfo extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       selectedMovie: null,
       movieTrailer: null
     }
   }
-  componentDidMount = () => {
 
-    APICalls.fetchSingleMovieData(this.props.movie.id,)
-    .then(data => this.state.selectedMovie ? this.setState({selectedMovie: null}) : this.setState({selectedMovie: data}))
+  componentDidMount() {
+    console.log('props', this.props)
+    APICalls.fetchSingleMovieData(this.props.id)
+    .then(data => this.setState({selectedMovie: data.movie}))
     .catch(error => this.setState({error: 'Oops! We are unable to display this movie'}))
 
-    APICalls.fetchMovieVideoData(this.props.movie.id,)
+    APICalls.fetchMovieVideoData(this.props.id)
     .then(data => this.setState({movieTrailer: data.videos}))
     .catch(error => this.setState({error: 'Oops! We are unable to display this trailer'}))
   }
@@ -35,45 +36,62 @@ class MovieInfo extends Component {
   // }
 
   render() {
+    if (this.state.selectedMovie === null || undefined ) {
       return (
-        <section className='movie-info-container' key={this.state.selectedMovie.id}>
-          <section className="banner">
-            <img
-              className="backdrop"
-              src={this.state.selectedMovie.backdrop_path}
-              alt={`backdrop for ${this.state.selectedMovie.title}`}
-            />
-            <h2 className="title">{this.state.selectedMovie.title}</h2>
-          </section>
-          <section className='movie-info'>
-            <section className='info-left'>
-              <div className='poster'>
-                <img src={this.state.selectedMovie.poster_path} alt={`movie poster for ${this.state.selectedMovie.title}`}/>
-                <p className='tagline'>{this.state.selectedMovie.tagline}</p>
-              </div>
-            </section>
-            <section className='info-right'>
-              <div className='right-wrapper'>
-                <p>Overview: {this.state.selectedMovie.overview}</p>
-                <p> Release Date: {this.state.selectedMovie.release_date}</p>
-                <p>Runtime: {this.state.selectedMovie.runtime} minutes</p>
-                <p> Average Rating: {Math.round(this.state.selectedMovie.average_rating * 100)/100} / 10</p>
-                <p className='genres'>Genre: {this.state.selectedMovie.genres}</p>
-                <p className='genres'>Genre: {this.state.displayGenres}</p>
-                <p>Budget: {this.state.selectedMovie.budget}</p>
-                <p>Revenue: {this.state.selectedMovie.revenue}</p>
-                <button onClick={this.state.displayHomePage}>Return Home</button>
-              </div>
-            </section>
-          </section>
-          <section className='trailer'>
-            {this.state.movieTrailer.length && <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.state.movieTrailer[0].key}`} title="YouTube video player">
-            </iframe>}
-          </section>
-        </section>
+        <div>
+          Loading
+        </div>
       )
     }
+    // deconstruct all of the properties within this.state.selectedMovie
+    const {
+      backdrop_path,
+      title,
+      poster_path,
+      release_date,
+      overview,
+      genres,
+      budget,
+      runtime,
+      tagline,
+      revenue,
+      average_rating } = this.state.selectedMovie
 
+      return (
+        <div>
+          { this.state.selectedMovie !== null &&
+            <section className='movie-info-container' key={this.state.selectedMovie.id}>
+              <section className="banner">
+                <img
+                  className="backdrop"
+                  src={this.state.selectedMovie.backdrop_path}
+                  alt={`backdrop`}/>
+                <h2 className="title">{title}</h2>
+              </section>
+              <section className='movie-info'>
+                <section className='info-left'>
+                  <div className='poster'>
+                    <img src={poster_path} alt={`movie poster`}/>
+                    <p className='tagline'>{tagline}</p>
+                  </div>
+                </section>
+                <section className='info-right'>
+                  <div className='right-wrapper'>
+                    <p>Overview: {overview}</p>
+                    <p> Release Date: {release_date}</p>
+                    <p>Runtime: {runtime} minutes</p>
+                    <p> Average Rating: {Math.round(average_rating * 100)/100} / 10</p>
+                    <p className='genres'>Genre: {genres}</p>
+                    <p>Budget: {budget}</p>
+                    <p>Revenue: {revenue}</p>
+                  </div>
+                </section>
+              </section>
+            </section>
+          }
+        </div>
+    )
+    }
 };
 
 
