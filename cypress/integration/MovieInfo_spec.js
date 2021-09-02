a6b228a3f95817c72af0c16a00cba5755ae4f10f
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-02 17:28:55 -0600

    Add basic movie info spec

--- cypress/integration/MovieInfo_spec.js
+++ cypress/integration/MovieInfo_spec.js
@@ -1,6 +1,8 @@
 describe('User Movie Info Page Flows', () => {
   beforeEach(() => {
     cy.loadSingleMovieInfo();
+    // cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919')
+    // .visit('http://localhost:3000/694919')
   });
 
 describe('Movie Info is Loading Status', () => {
@@ -18,17 +20,21 @@ describe('Movie Info is Loading Status', () => {
     it('Should render all data for the movie info display', () => {
       cy.get('.title').contains('Money Plane')
       .get('.backdrop').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg')
-      
+
 
     });
 
-    // it('Should render an iframe with an embedded youtube video that can be clicked', () => {
-        
-    // })
+    it('Should render an iframe with an embedded youtube video that can be clicked', () => {
+        cy.get('iframe').should('have.attr', 'src', 'https://www.youtube.com/embed/aETz_dRDEys')
+    })
 
-    // it('It should display a go back button that when clicked returns user to home page', () => {
-      
-    // })
+    it('It should display a go back button that when clicked returns user to home page', () => {
+      cy.get('.home-btn')
+            .click()
+            // cy.loadMain()
+            .url().should('eq', 'http://localhost:3000/')
+            .url().should('not.eq', 'http://localhost:3000/694919');
+    })
 
   });
 
