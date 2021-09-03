a6afe15e8b3223209df3fd8d33da673202dea870
Mae Duphorne <maelduphorne@gmail.com>
2021-09-03 11:26:43 -0600

    Create search component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,6 +43,7 @@ class App extends Component {
       <main className='App'>
         <h1>Rancid Tomatillos</h1>
         {this.state.error && <h2>{this.state.error}</h2>}
+        <Search/>
         <Route exact path= '/' render= {() => <MoviesArea movies={this.state.movies} displayMovie={this.displayMovie}/> }/>
 
         <Route exact path= '/:id' render={({match}) => {
